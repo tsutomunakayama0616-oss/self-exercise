8b8b754f40a69539ed6f05a97283fa1c79e35413
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-09 00:22:17 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,45 @@
+const CACHE_NAME = "selftraining-cache-v1";
+const URLS_TO_CACHE = [
+  "index.html",
+  "exercises.html",
+  "exercise-detail.html",
+  "check.html",
+  "result.html",
+  "progress.html",
+  "guide.html",
+  "style.css",
+  "tabbar.css",
+  "card.css",
+  "camera.css",
+  "script.js",
+  "exercises.js",
+  "analysis.js",
+  "progress.js",
+  "result.js",
+  "icon-152.png",
+  "icon-192.png",
+  "icon-512.png",
+  "manifest.json"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
 self.addEventListener("activate", event => {
-  console.log("Service Worker activated");
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
 });
 
 self.addEventListener("fetch", event => {
-  // 必要に応じてキャッシュ処理を追加
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
