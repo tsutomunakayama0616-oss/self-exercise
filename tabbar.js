3c8ef63f9821bcf23791463a5eb144b0714afcbf
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-09 01:17:18 +0900

    Update tabbar.js

--- tabbar.js
+++ tabbar.js
@@ -1,5 +1,32 @@
-fetch("tabbar.html")
-  .then(res => res.text())
-  .then(html => {
-    document.body.insertAdjacentHTML("beforeend", html);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const tabbar = `
+    <nav class="tabbar">
+      <a href="index.html">
+        <img src="home.png" class="tab-icon">
+        <span>ホーム</span>
+      </a>
+
+      <a href="exercises.html">
+        <img src="exercise.png" class="tab-icon">
+        <span>エクササイズ</span>
+      </a>
+
+      <a href="check.html">
+        <img src="form.png" class="tab-icon">
+        <span>フォーム</span>
+      </a>
+
+      <a href="progress.html">
+        <img src="progress.png" class="tab-icon">
+        <span>進捗</span>
+      </a>
+
+      <a href="guide.html">
+        <img src="guide.png" class="tab-icon">
+        <span>ガイド</span>
+      </a>
+    </nav>
+  `;
+
+  document.body.insertAdjacentHTML("beforeend", tabbar);
+});
