515393e0f912a7b01eca8dec3cb2cad526b6e36c
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-09 00:13:58 +0900

    Create result.js

---
+++ result.js
@@ -0,0 +1,7 @@
+document.getElementById("abduction").textContent = 25;
+document.getElementById("knee").textContent = 45;
+document.getElementById("pelvis").textContent = 10;
+document.getElementById("trunk").textContent = 5;
+
+document.getElementById("good").innerHTML = "<li>姿勢が安定しています</li>";
+document.getElementById("bad").innerHTML = "<li>膝の角度が浅いです</li>";
