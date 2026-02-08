42f20886d6a610d3c68dbadc8c7d1f80e154b834
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-09 00:13:33 +0900

    Create progress.js

---
+++ progress.js
@@ -0,0 +1,8 @@
+const progressList = document.getElementById("progress-list");
+
+if (progressList) {
+  progressList.innerHTML = `
+    <div class="card">1月：フォーム改善 20%</div>
+    <div class="card">2月：フォーム改善 35%</div>
+  `;
+}
